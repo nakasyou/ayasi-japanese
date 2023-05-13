822893c74ea7cf93a51a89969a75711e7a9bbce7
Shotaro Nakamura <79000684+nakasyou@users.noreply.github.com>
2023-05-13 15:04:07 +0900

    Update convert.ts

--- ayasi/convert.ts
+++ ayasi/convert.ts
@@ -1,9 +1,10 @@
 export default (text: string): string => {
   const data: Record<string,string> = {
     "語": "语",
+    "し": "レ"
   }
   for(const [pure, ayasi] of Object.entries(data)){
-    text = text.replaceAll(pure, (raw) => {
+    text = text.replaceAll(new RegExp(pure), (raw) => {
       return (Math.random()>0.5) ? ayasi : raw
     })
   }
