af0f21dfcf4d168d13bbe75b81a1f70ec1c66b48
Shotaro Nakamura <79000684+nakasyou@users.noreply.github.com>
2023-05-13 14:51:16 +0900

    Create main.ts

---
+++ main.ts
@@ -0,0 +1,13 @@
+import { serve } from 'https://deno.land/std@0.187.0/http/server.ts'
+import { Hono } from 'https://deno.land/x/hono@v3.2.0-rc.2/mod.ts'
+
+import ayasi from "./ayasi/index.ts"
+
+const app = new Hono()
+
+app.get('/', c => c.json({
+  title: "怪レい日本语",
+}))
+app.get('/api', ayasi)
+
+serve(app.fetch)
