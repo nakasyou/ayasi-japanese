c28205e4bc28084a80c6f0ab6191ce9d781475a0
Shotaro Nakamura <79000684+nakasyou@users.noreply.github.com>
2023-05-13 15:13:16 +0900

    Update convert.ts

--- ayasi/convert.ts
+++ ayasi/convert.ts
@@ -3,6 +3,8 @@ export default (text: string): string => {
     "語": "语",
     "し": "レ",
     "きさま|あなた|貴方|きみ": "贵樣",
+    "すごい|えぐい|やばい|めっちゃ|大きな|": "極度",
+    "る": "ゑ",
   }
   for(const [pure, ayasi] of Object.entries(data)){
     text = text.replaceAll(new RegExp(pure,"g"), (raw) => {
