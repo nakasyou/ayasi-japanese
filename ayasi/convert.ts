4699c5815c3019e5f5c589bee3ab6985b199cc39
Shotaro Nakamura <79000684+nakasyou@users.noreply.github.com>
2023-05-13 14:58:09 +0900

    Update convert.ts

--- ayasi/convert.ts
+++ ayasi/convert.ts
@@ -1,3 +1,9 @@
 export default (text: string): string => {
+  const data: Record<string,string> = {
+    "語": "语",
+  }
+  for(const [pure, ayasi] of Object.entries(data)){
+    text = text.replaceAll(pure,ayasi)
+  }
   return text
 }
