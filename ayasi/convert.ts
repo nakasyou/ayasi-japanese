6af78849513e709c2b998f178c819dc0f0d17cc6
Shotaro Nakamura <79000684+nakasyou@users.noreply.github.com>
2023-05-13 15:16:38 +0900

    Update convert.ts

--- ayasi/convert.ts
+++ ayasi/convert.ts
@@ -1,10 +1,14 @@
 export default (text: string): string => {
   const data: Record<string,string> = {
     "語": "语",
-    "し": "レ",
     "きさま|あなた|貴方|きみ": "贵樣",
     "すごい|えぐい|やばい|めっちゃ|大きな": "極度",
+    
+    // ひらがな
+    "し": "レ",
     "る": "ゑ",
+    "ら": "う",
+    "か", "カ",
   }
   for(const [pure, ayasi] of Object.entries(data)){
     text = text.replaceAll(new RegExp(pure,"g"), (raw) => {
